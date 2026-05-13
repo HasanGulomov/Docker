218b4447a6fb46978ac86956caeed846f934311b
HasanGulomov <gulomovhasan194@gmail.com>
2026-05-13 14:54:04 +0500

    Ready

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Mana shu qator Docker bilan bog'lanish uchun shart!
+    port: 5173,
+    watch: {
+      usePolling: true, // Fayllardagi o'zgarishlarni Dockerda tezroq sezish uchun
+    },
+  },
 })
